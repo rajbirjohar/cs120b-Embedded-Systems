91d098b62e3442f454672712244eebb817fe7201
Shan Rajbir Johar <sjoha005@wch136-24.cs.ucr.edu>
2021-06-28 16:37:20 -0700

    Completed Lab 2 part 2

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -16,32 +16,32 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
     
-    unsigned char availParking; // tracks number of free spaces
+    unsigned char cntavail; // tracks number of free spaces
     unsigned char parkingSpace1 = 0x00; 
     unsigned char parkingSpace2 = 0x01;  
     unsigned char parkingSpace3 = 0x02; 
     unsigned char parkingSpace4 = 0x03; 
 
     while(1){
-        availParking = 0x04;
+        cntavail = 0x04;
         parkingSpace1 = PINA & 0x01;
         parkingSpace2 = PINA & 0x02;
         parkingSpace3 = PINA & 0x04;
         parkingSpace4 = PINA & 0x08;
 
         if(parkingSpace1 == 0x01){
-	    availParking--;
+	    cntavail--;
 	}
         if(parkingSpace2 == 0x02){
-	    availParking--;
+	    cntavail--;
 	}	
         if(parkingSpace3 == 0x04){
-	    availParking--;
+	    cntavail--;
  	}
         if(parkingSpace4 == 0x08){
-	    availParking--;
+	    cntavail--;
 	}
-	PORTC = availParking;
+	PORTC = cntavail;
     }
     return 1;
 }
