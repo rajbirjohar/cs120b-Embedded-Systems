97241474bb02c43b56b573b12dfd2d7251bd9097
r-jo <rajbir@rajbir.io>
2021-07-06 17:18:39 -0700

    Created Lab 6 part 2

--- lab_chip/source/main.c
+++ lab_chip/source/main.c
@@ -18,8 +18,13 @@ enum LEDStates
     LEDStart,
     LED1State,
     LED2State,
-    LED3State
+    LED3State,
+    MiddleState,
+    PressedState,
+    RestartState
 } LEDState;
+
+unsigned char button = 0x00;
 
 void Tick()
 {
@@ -30,15 +35,69 @@ void Tick()
         break;
 
     case LED1State:
-        LEDState = LED2State;
+        if (button)
+        {
+            LEDState = PressedState;
+        }
+        else
+        {
+            LEDState = LED2State;
+        }
         break;
 
     case LED2State:
-        LEDState = LED3State;
+        if (button)
+        {
+            LEDState = PressedState;
+        }
+        else
+        {
+            LEDState = LED3State;
+        }
         break;
 
     case LED3State:
-        LEDState = LED1State;
+        if (button)
+        {
+            LEDState = PressedState;
+        }
+        else
+        {
+            LEDState = MiddleState;
+        }
+        break;
+
+    case MiddleState:
+        if (button)
+        {
+            LEDState = PressedState;
+        }
+        else
+        {
+            LEDState = LED1State;
+        }
+        break;
+
+    case PressedState:
+        if (button)
+        {
+            LEDState = PressedState;
+        }
+        else
+        {
+            LEDState = RestartState;
+        }
+        break;
+
+    case RestartState:
+        if (!button)
+        {
+            LEDState = RestartState;
+        }
+        else
+        {
+            LEDState = LED1State;
+        }
         break;
     }
 
@@ -49,11 +108,16 @@ void Tick()
         break;
 
     case LED2State:
+    case MiddleState:
         PORTB = 0x02;
         break;
 
     case LED3State:
         PORTB = 0x04;
+        break;
+
+    case PressedState:
+    case RestartState:
         break;
 
     default:
@@ -63,15 +127,18 @@ void Tick()
 
 int main(void)
 {
+    DDRA = 0x00;
+    PORTA = 0xFF;
     DDRB = 0xFF;
     PORTB = 0x00;
 
-    TimerSet(100);
+    TimerSet(30);
     TimerOn();
     LEDState = LEDStart;
 
     while (1)
     {
+        button = ~PINA & 0x01;
         Tick();
         while (!TimerFlag)
             ;
