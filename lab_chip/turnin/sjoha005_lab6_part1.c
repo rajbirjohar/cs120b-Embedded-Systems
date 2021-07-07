0d29559712ae99bf150964425215cbae9690023a
r-jo <rajbir@rajbir.io>
2021-07-06 17:18:20 -0700

    Completed Lab 6 part 1

---
+++ lab_chip/turnin/sjoha005_lab6_part1.c
@@ -0,0 +1,82 @@
+/*	Author: sjoha005
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include "timer.h"
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum LEDStates
+{
+    LEDStart,
+    LED1State,
+    LED2State,
+    LED3State
+} LEDState;
+
+void Tick()
+{
+    switch (LEDState)
+    {
+    case LEDStart:
+        LEDState = LED1State;
+        break;
+
+    case LED1State:
+        LEDState = LED2State;
+        break;
+
+    case LED2State:
+        LEDState = LED3State;
+        break;
+
+    case LED3State:
+        LEDState = LED1State;
+        break;
+    }
+
+    switch (LEDState)
+    {
+    case LED1State:
+        PORTB = 0x01;
+        break;
+
+    case LED2State:
+        PORTB = 0x02;
+        break;
+
+    case LED3State:
+        PORTB = 0x04;
+        break;
+
+    default:
+        break;
+    }
+}
+
+int main(void)
+{
+    DDRB = 0xFF;
+    PORTB = 0x00;
+
+    TimerSet(100);
+    TimerOn();
+    LEDState = LEDStart;
+
+    while (1)
+    {
+        Tick();
+        while (!TimerFlag)
+            ;
+        TimerFlag = 0;
+    }
+
+    return 1;
+}
