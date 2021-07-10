0785a9d346c228701f65739cc4be534e4509d543
r-jo <rajbir@rajbir.io>
2021-07-10 07:30:14 -0700

    Initial commit Lab 8 part 1

--- lab_chip/source/main.c
+++ lab_chip/source/main.c
@@ -12,63 +12,178 @@
 #include "simAVRHeader.h"
 #endif
 
-void ADC_init()
+// 0.954 hz is lowest frequency possible with this function,
+// based on settings in PWM_on()
+// Passing in 0 as the frequency will stop the speaker from generating sound
+void set_PWM(double frequency)
 {
-    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
-    // ADEN: setting this bit enables analog-to-digital conversion
-    // ADSC: setting this bit starts the first conversion
-    // ADATE: setting this bit enables auto-triggering. Since we are
-    //		  in Free Running Mode, a new conversion will trigger whenever
-    //		  the previous conversion completes.
+    static double current_frequency; // Keeps track of the currently set frequency
+    // Will only update the registers when the frequency changes, otherwise allows
+    // music to play uninterrupted.
+    if (frequency != current_frequency)
+    {
+        if (!frequency)
+        {
+            TCCR3B &= 0x08;
+        } // stops timer/counter
+        else
+        {
+            TCCR3B |= 0x03;
+        } // resumes/continues timer/counter
+
+        // prevents OCR3A from overflowing, using prescaler 64
+        // 0.954 Hz is smallest frequency that will not result in overflow
+        if (frequency < 0.954)
+        {
+            OCR3A = 0xFFFF;
+        }
+
+        // prevents OCR3A from underflowing, using prescaler 64
+        // 31250 is largest frequency that will not result in underflow
+        else if (frequency > 31250)
+        {
+            OCR3A = 0x0000;
+        }
+
+        // set OCR3A based on desired frequency
+        else
+        {
+            OCR3A = (short)(8000000 / (128 * frequency)) - 1;
+        }
+
+        TCNT3 = 0; // resets counter
+        current_frequency = frequency;
+    }
+}
+
+void PWM_on()
+{
+    TCCR3A = (1 << COM3A0);
+    // COM3A0: Toggle PB3 on compare match between counter and OCR3A
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    // WGM32: When counter (TCNT3) matches OCR3A, reset counter
+    // CS31 & CS30: Set a prescaler of 64
+    set_PWM(0);
+}
+
+void PWM_off()
+{
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
+enum soundStates
+{
+    Sound_Start,
+    WaitState,
+    C4Sound,
+    D4Sound,
+    E4Sound
+} soundState;
+
+unsigned char C4 = 0x00, D4 = 0x00, E4 = 0x00;
+
+void Tick()
+{
+    switch (soundState)
+    {
+    case Sound_Start:
+        soundState = WaitState;
+        break;
+
+    case WaitState:
+        if (C4 && !D4 && !E4)
+        {
+            soundState = C4Sound;
+        }
+        else if (!C4 && D4 && !E4)
+        {
+            soundState = D4Sound;
+        }
+        else if (!C4 && !D4 && E4)
+        {
+            soundState = E4Sound;
+        }
+        else
+        {
+            soundState = WaitState;
+        }
+        break;
+
+    case C4Sound:
+        if (C4 && !D4 && !E4)
+        {
+            soundState = C4Sound;
+        }
+        else
+        {
+            soundState = WaitState;
+        }
+        break;
+
+    case D4Sound:
+        if (!C4 && D4 && !E4)
+        {
+            soundState = D4Sound;
+        }
+        else
+        {
+            soundState = WaitState;
+        }
+        break;
+
+    case E4Sound:
+        if (!C4 && !D4 && E4)
+        {
+            soundState = E4Sound;
+        }
+        else
+        {
+            soundState = WaitState;
+        }
+        break;
+    }
+
+    switch (soundState)
+    {
+    case WaitState:
+        set_PWM(0);
+        break;
+
+    case C4Sound:
+        set_PWM(261.63);
+        break;
+
+    case D4Sound:
+        set_PWM(293.66);
+        break;
+
+    case E4Sound:
+        set_PWM(329.63);
+        break;
+
+    default:
+        break;
+    }
 }
 
 int main(void)
 {
     DDRA = 0x00;
     PORTA = 0xFF;
-    DDRB = 0xFF;
+    DDRB = 0x40;
     PORTB = 0x00;
 
-    unsigned short my_short = 0x00;
-
-    ADC_init();
+    PWM_on();
+    soundState = Sound_Start;
 
     while (1)
     {
-        my_short = ADC;
+        C4 = ~PINA & 0x01;
+        D4 = ~PINA & 0x02;
+        E4 = ~PINA & 0x04;
 
-        if (my_short >= 475)
-        {
-            PORTB = 0xFF;
-        }
-        else if (my_short >= (475 / 2))
-        {
-            PORTB = 0X7F;
-        }
-        else if (my_short >= (475 / 3))
-        {
-            PORTB = 0x3F;
-        }
-        else if (my_short >= (475 / 4))
-        {
-            PORTB = 0x1F;
-        }
-        else if (my_short >= (475 / 5))
-        {
-            PORTB = 0x0F;
-        }
-        else if (my_short >= (475 / 6))
-        {
-            PORTB = 0x07;
-        }
-        else if (my_short >= (475 / 7))
-        {
-            PORTB = 0x03;
-        }
-        else
-        {
-            PORTB = 0x01;
-        }
+        Tick();
     }
 
     return 1;
