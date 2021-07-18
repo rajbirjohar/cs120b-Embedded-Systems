396732b81ecca924635c637b5ed946b99f7e325d
rajbirjohar <github@rajbir.io>
2021-07-18 15:39:35 -0700

    Initial commit Lab 10 part 1

--- lab_chip/source/main.c
+++ lab_chip/source/main.c
@@ -32,17 +32,169 @@ void transmit_data(unsigned char data)
     PORTC = 0x00;
 }
 
+unsigned char incrementButton = 0x00, decrementButton = 0x00, tempC = 0x00;
+
+enum buttonStates
+{
+    startButton,
+    InitialState,
+    WaitState,
+    WaitState1,
+    WaitState2,
+    IncrementState,
+    DecrementState,
+    ResetState
+} buttonState;
+
+unsigned char IncrementStateButton = 0x00, DecrementStateButton = 0x00, tempC = 0x00;
+
+void Tick()
+{
+    switch (buttonState)
+    {
+    case startButton:
+        buttonState = InitialState;
+        break;
+
+    case InitialState:
+        if (IncrementStateButton && DecrementStateButton)
+        {
+            buttonState = ResetState;
+        }
+        else if (IncrementStateButton)
+        {
+            buttonState = IncrementState;
+        }
+        else if (DecrementStateButton)
+        {
+            buttonState = DecrementState;
+        }
+        else
+        {
+            buttonState = InitialState;
+        }
+        break;
+
+    case IncrementState:
+    case WaitState1:
+        if (IncrementStateButton && DecrementStateButton)
+        {
+            buttonState = ResetState;
+        }
+        else if (!IncrementStateButton && !DecrementStateButton)
+        {
+            buttonState = InitialState;
+        }
+        else if (DecrementStateButton)
+        {
+            buttonState = DecrementState;
+        }
+        else
+        {
+            buttonState = WaitState1;
+        }
+        break;
+
+    case DecrementState:
+    case WaitState2:
+        if (IncrementStateButton && DecrementStateButton)
+        {
+            buttonState = ResetState;
+        }
+        else if (!IncrementStateButton && !DecrementStateButton)
+        {
+            buttonState = InitialState;
+        }
+        else if (IncrementStateButton)
+        {
+            buttonState = IncrementState;
+        }
+        else
+        {
+            buttonState = WaitState2;
+        }
+        break;
+
+    case ResetState:
+        if (!IncrementStateButton && !DecrementStateButton)
+        {
+            buttonState = InitialState;
+        }
+        else
+        {
+            buttonState = WaitState;
+        }
+        break;
+
+    case WaitState:
+        if (!IncrementStateButton && !DecrementStateButton)
+        {
+            buttonState = InitialState;
+        }
+        else if (IncrementStateButton)
+        {
+            buttonState = IncrementState;
+        }
+        else if (DecrementStateButton)
+        {
+            buttonState = DecrementState;
+        }
+        else
+        {
+            buttonState = WaitState;
+        }
+        break;
+    }
+
+    switch (buttonState)
+    {
+    case InitialState:
+    case WaitState:
+    case WaitState1:
+    case WaitState2:
+        break;
+
+    case IncrementState:
+        if (tempC < 9)
+        {
+            tempC = tempC + 1;
+        }
+        break;
+
+    case DecrementState:
+        if (tempC > 0)
+        {
+            tempC = tempC - 1;
+        }
+        break;
+
+    case ResetState:
+        tempC = 0;
+        break;
+
+    default:
+        break;
+    }
+}
+
 int main(void)
 {
     DDRA = 0x00;
     PORTA = 0xFF;
     DDRC = 0xFF;
     PORTC = 0x00;
-    unsigned char tempC = 0x0F;
-    transmit_data(tempC);
+
+    buttonState = startButton;
+    tempC = 0x07;
+
     while (1)
     {
-        ;
+        IncrementStateButton = PINA & 0x01;
+        DecrementStateButton = PINA & 0x02;
+        DecrementStateButton = (DecrementStateButton >> 1);
+        Tick();
+        transmit_data(tempC);
     }
+
     return 1;
 }
